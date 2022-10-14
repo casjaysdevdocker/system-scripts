a4eade321de9275d40cba7c2b5748ecc8cfbabd2
casjay <git-admin@casjaysdev.com>
2022-10-14 14:03:26 -0400

    🗃️ Committing everything that changed 🗃️

--- bin/entrypoint-system-scripts.sh
+++ bin/entrypoint-system-scripts.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202210080911-git
+##@Version           :  202210141357-git
 # @@Author           :  Jason Hempstead
 # @@Contact          :  jason@casjaysdev.com
-# @@License          :  WTFPL
+# @@License          :  LICENSE.md
 # @@ReadME           :  entrypoint-system-scripts.sh --help
 # @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
-# @@Created          :  Saturday, Oct 08, 2022 09:11 EDT
+# @@Created          :  Friday, Oct 14, 2022 13:57 EDT
 # @@File             :  entrypoint-system-scripts.sh
 # @@Description      :
 # @@Changelog        :  New script
@@ -20,76 +20,156 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
 [ -n "$DEBUG" ] && set -x
-set -o pipefail
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 APPNAME="$(basename "$0" 2>/dev/null)"
-VERSION="202210080911-git"
+VERSION="202210141357-git"
 HOME="${USER_HOME:-$HOME}"
 USER="${SUDO_USER:-$USER}"
 RUN_USER="${SUDO_USER:-$USER}"
 SCRIPT_SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set functions
-__version() { echo -e ${GREEN:-}"$VERSION"${NC:-}; }
-__find() { ls -A "$*" 2>/dev/null; }
+__find() { find "$1" -mindepth 1 -type f,d 2>/dev/null | grep '^' || return 10; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# colorization
-[ -n "$SHOW_RAW" ] || printf_color() { echo -e '\t\t'${2:-}"${1:-}${NC}"; }
+__pid_grep() { ps aux | grep -F "$1" | grep -v 'grep' || return 1; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-__exec_bash() {
-  local cmd="${*:-/bin/bash}"
+__start_shell() {
+  local l="$(which zsh || which bash || which sh || echo 'false')"
+  echo "$l"
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__exec_command() {
   local exitCode=0
+  local cmd="${*:-$(__start_shell) -l}"
   echo "Executing command: $cmd"
-  $cmd || exitCode=10
+  eval $cmd || exitCode=10
+  [ "$exitCode" = 0 ] || exitCode=10
   return ${exitCode:-$?}
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Define default variables
-TZ="${TZ:-America/New_York}"
-HOSTNAME="${HOSTNAME:-casjaysdev-bin}"
-BIN_DIR="${BIN_DIR:-/usr/local/bin}"
-DATA_DIR="${DATA_DIR:-$(__find /data/ 2>/dev/null | grep '^' || false)}"
-CONFIG_DIR="${CONFIG_DIR:-$(__find /config/ 2>/dev/null | grep '^' || false)}"
-CONFIG_COPY="${CONFIG_COPY:-false}"
+__heath_check() {
+  local status=0
+  local health="Good"
+  #__pid_grep "bash" || status=$(($status + 1))
+  #curl -q -LSsf -o /dev/null -s -w "200" "http://localhost/server-health" || status=$(($status + 1))
+  [ "$status" -eq 0 ] || health="Errors reported see docker logs --follow $CONTAINER_NAME"
+  echo "$(uname -s) $(uname -m) is running and the health is: $health"
+  return ${status:-$?}
+}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Additional variables
+# Additional functions
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Export variables
-export TZ HOSTNAME
+# Define default variables - do not change these - redifine with -e or set under Additional
+LANG="${LANG:-C.UTF-8}"
+DOMANNAME="${DOMANNAME:-}"
+TZ="${TZ:-America/New_York}"
+HTTP_PORT="${HTTP_PORT:-80}"
+HTTPS_PORT="${HTTPS_PORT:-443}"
+SERVICE_PORT="${SERVICE_PORT:-}"
+HOSTNAME="${HOSTNAME:-casjaysdev-bin}"
+HOSTADMIN="${HOSTADMIN:-root@${DOMANNAME:-$HOSTNAME}}"
+SSL_ENABLED="${SSL_ENABLED:-false}"
+SSL_DIR="${SSL_DIR:-/config/ssl}"
+SSL_CA="${SSL_CA:-$SSL_DIR/ca.crt}"
+SSL_KEY="${SSL_KEY:-$SSL_DIR/server.key}"
+SSL_CERT="${SSL_CERT:-$SSL_DIR/server.crt}"
+LOCAL_BIN_DIR="${LOCAL_BIN_DIR:-/usr/local/bin}"
+DEFAULT_DATA_DIR="${DEFAULT_CONF_DIR:-/usr/local/share/template-files/data}"
+DEFAULT_CONF_DIR="${DEFAULT_CONF_DIR:-/usr/local/share/template-files/config}"
+DEFAULT_TEMPLATE_DIR="${DEFAULT_TEMPLATE_DIR:-/usr/local/share/template-files/defaults}"
+CONTAINER_IP_ADDRESS="$(ip a | grep 'inet' | grep -v '127.0.0.1' | awk '{print $2}' | sed 's|/*||g')"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional variables and variable overrides
+MY_VAR=""
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# export variables
+export MY_VAR
+export LANG TZ DOMANNAME HOSTNAME HOSTADMIN SSL_ENABLED SSL_DIR SSL_CA SSL_KEY
+export SSL_DIR HTTP_PORT HTTPS_PORT LOCAL_BIN_DIR DEFAULT_CONF_DIR CONTAINER_IP_ADDRESS
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # import variables from file
 [ -f "/root/env.sh" ] && . "/root/env.sh"
+[ -f "/config/env.sh" ] && "/config/env.sh"
 [ -f "/config/.env.sh" ] && . "/config/.env.sh"
-[ -f "/root/env.sh" ] && [ ! -f "/config/.env.sh" ] && cp -Rf "/root/env.sh" "/config/.env.sh"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set timezone
-[ -n "${TZ}" ] && echo "${TZ}" >/etc/timezone
+[ -n "${TZ}" ] && echo "${TZ}" >"/etc/timezone"
 [ -f "/usr/share/zoneinfo/${TZ}" ] && ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set hostname
 if [ -n "${HOSTNAME}" ]; then
-  echo "${HOSTNAME}" >/etc/hostname
-  echo "127.0.0.1 ${HOSTNAME} localhost ${HOSTNAME}.local" >/etc/hosts
+  echo "${HOSTNAME}" >"/etc/hostname"
+  echo "127.0.0.1 ${HOSTNAME} localhost ${HOSTNAME}.local" >"/etc/hosts"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Add domain to hosts file
+if [ -n "$DOMANNAME" ]; then
+  echo "${HOSTNAME}.${DOMANNAME:-local}" >"/etc/hostname"
+  echo "127.0.0.1 ${HOSTNAME} localhost ${HOSTNAME}.local" >"/etc/hosts"
+  echo "${CONTAINER_IP_ADDRESS:-127.0.0.1} ${HOSTNAME}.${DOMANNAME}" >>"/etc/hosts"
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Delete any gitkeep files
-[ -n "${CONFIG_DIR}" ] && { [ -d "${CONFIG_DIR}" ] && rm -Rf "${CONFIG_DIR}/.gitkeep" || mkdir -p "/config/"; }
-[ -n "${DATA_DIR}" ] && { [ -d "${DATA_DIR}" ] && rm -Rf "${DATA_DIR}/.gitkeep" || mkdir -p "/data/"; }
-[ -n "${BIN_DIR}" ] && { [ -d "${BIN_DIR}" ] && rm -Rf "${BIN_DIR}/.gitkeep" || mkdir -p "/bin/"; }
+[ -d "/data" ] && rm -Rf "/data/.gitkeep" "/data"/*/*.gitkeep
+[ -d "/config" ] && rm -Rf "/config/.gitkeep" "/data"/*/*.gitkeep
+[ -f "/usr/local/bin/.gitkeep" ] && rm -Rf "/usr/local/bin/.gitkeep"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Copy config files to /etc
-if [ -n "${CONFIG_DIR}" ] && [ "${CONFIG_COPY}" = "true" ]; then
-  for config in ${CONFIG_DIR}; do
-    if [ -d "/config/$config" ]; then
-      [ -d "/etc/$config" ] || mkdir -p "/etc/$config"
-      cp -Rf "/config/$config/." "/etc/$config/"
-    elif [ -f "/config/$config" ]; then
-      cp -Rf "/config/$config" "/etc/$config"
+# Create directories
+[ -d "/etc/ssl" ] || mkdir -p "/etc/ssl"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Create files
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ "$SSL_ENABLED" = "true" ] || [ "$SSL_ENABLED" = "yes" ]; then
+  if [ -f "/config/ssl/server.crt" ] && [ -f "/config/ssl/server.key" ]; then
+    export SSL_ENABLED="true"
+    if [ -n "$SSL_CA" ] && [ -f "$SSL_CA" ]; then
+      mkdir -p "/etc/ssl/certs"
+      cat "$SSL_CA" >>"/etc/ssl/certs/ca-certificates.crt"
+    fi
+  else
+    [ -d "$SSL_DIR" ] || mkdir -p "$SSL_DIR"
+    create-ssl-cert
+  fi
+  type update-ca-certificates &>/dev/null && update-ca-certificates
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+[ -f "$SSL_CA" ] && cp -Rfv "$SSL_CA" "/etc/ssl/ca.crt"
+[ -f "$SSL_KEY" ] && cp -Rfv "$SSL_KEY" "/etc/ssl/server.key"
+[ -f "$SSL_CERT" ] && cp -Rfv "$SSL_CERT" "/etc/ssl/server.crt"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup bin directory
+if [ -d "/data/bin" ]; then
+  for create_bin in /data/bin/*; do
+    create_bin_name="$(basename "$create_bin")"
+    ln -sf "$create_bin" "/usr/local/bin/$create_bin_name"
+  done
+  unset create_bin create_bin_name
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Create default config
+if [ -n "$DEFAULT_TEMPLATE_DIR" ] && [ -d "$DEFAULT_TEMPLATE_DIR" ]; then
+  for create_template in "$DEFAULT_TEMPLATE_DIR"/*; do
+    create_template_name="$(basename "$create_template")"
+    if [ ! -e "/config/$create_template_name" ]; then
+      cp -Rf "$create_template" "/config/$create_template_name"
     fi
   done
+  unset create_template create_template_name
 fi
-[ -f "/etc/.env.sh" ] && rm -Rf "/etc/.env.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Create config symlinks
+if [ -d "/config" ]; then
+  for create_conf in /config/*; do
+    create_conf_name="$(basename "$create_conf")"
+    if [ -e "/etc/$create_conf_name" ]; then
+      rm -Rf "/etc/${create_conf_name:?}"
+      ln -sf "$create_conf" "/etc/$create_conf_name"
+    fi
+  done
+  unset create_conf create_conf_name
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Additional commands
 
@@ -99,29 +179,30 @@ case "$1" in
   echo 'Docker container for '$APPNAME''
   echo "Usage: $APPNAME [healthcheck, bash, command]"
   echo "Failed command will have exit code 10"
-  echo
-  exitCode=$?
+  echo ""
+  exit ${exitCode:-$?}
   ;;
 
 healthcheck) # Docker healthcheck
-  echo "$(uname -s) $(uname -m) is running"
-  echo _other_commands here
-  exitCode=$?
+  __heath_check || exitCode=10
+  exit ${exitCode:-$?}
   ;;
 
 */bin/sh | */bin/bash | bash | shell | sh) # Launch shell
   shift 1
-  __exec_bash "${@:-/bin/bash}"
-  exitCode=$?
+  __exec_command "${@:-/bin/bash}"
+  exit ${exitCode:-$?}
   ;;
 
 *) # Execute primary command
   if [ $# -eq 0 ]; then
-    __exec_bash "/bin/bash"
+    echo "Container ip address is: $CONTAINER_IP_ADDRESS"
+    __exec_command "/bin/bash" -l
+    exit ${exitCode:-$?}
   else
-    __exec_bash "/bin/bash"
+    __exec_command "$@"
+    exitCode=$?
   fi
-  exitCode=$?
   ;;
 esac
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
